8c0486bd3a7d795978e03bcca59d00f2a9558073
Frida010 <>
2023-10-25 13:20:16 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import dj_database_url
+from django.contrib.messages import constants as messages
 if os.path.isfile("env.py"):
     import env
 
@@ -16,8 +17,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# SECURITY WARNING: do't run with debug turned on in production!
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ["8000-frida010-django-blog2-vqy032ed1d.us2.codeanyapp.com",
                  "django-blog-2.herokuapp.com", "django-blog-2-0f1d550d26af.herokuapp.com"]
@@ -47,6 +50,14 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 CRISPY_TEMPALTE_PACK = 'bootstrap4'
 
